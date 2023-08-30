6deb48ebebd983a7b7d3a8ba03877616aa39bdb3
Nicolas Braun <braunico@gmail.com>
2023-08-30 15:54:04 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -29,12 +29,32 @@ echo "source ~/.oh-my-zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlightin
 sed -io 's/^plugins=.*/plugins=(autojump git brew common-aliases zsh-autosuggestions copydir copyfile encode64 node osx sublime tmux xcode pod docker git-extras git-prompt)/' ~/.zshrc
 sed -io 's/^ZSH_THEME.*/ZSH_THEME="dpoggi"/' ~/.zshrc
 
-# echo "Install VIM settings"
-# git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 
 echo "Install Homebrew Packages from BrewFile, this might take a while"
 brew tap homebrew/bundle
-brew bundle
+brew bundle --file=Brewfile_basics
+
+read -p "Would you like to install personal apps? (Spotify, VLC...) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  brew bundle --file=Brewfile_personal
+fi
+
+read -p "Would you like to install Works apps? (Dev stuff mostly) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  brew bundle --file=Brewfile_work
+fi
+
+read -p "Would you like to install Gaming apps? (Bnet steam) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  brew bundle --file=Brewfile_gaming
+fi
+
 echo "Cleaning up brew"
 brew cask cleanup
 brew cleanup
@@ -53,14 +73,9 @@ git config --global core.editor $(which code)
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
 git config --global alias.tree "log --graph --full-history --all --color --date=short --pretty=format:'%Cred%x09%h %Creset%ad%Cblue%d %Creset %s %C(bold)(%an)%Creset'"
 
+# TODO
 # echo "Copying dotfiles from Github"
 # cd ~
 # git clone git@github.com:bradp/dotfiles.git .dotfiles
 # cd .dotfiles
 # sh symdotfiles
-
-# echo "Enabling Services"
-# open /Applications/Alfred\ 4.app
-# open /Applications/Gas\ Mask.app
-# open /Applications/Clipy.app
-# open /Applications/Rectangle.app
