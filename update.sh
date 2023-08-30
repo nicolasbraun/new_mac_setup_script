35335af93796b3cd5062ec7e29c59ff8bf3ac106
Nicolas Braun <braunico@gmail.com>
2023-08-30 15:08:29 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -7,11 +7,3 @@ brew update && brew upgrade && brew cleanup && brew cask cleanup; brew bundle; b
 
 echo "Updating Oh My Zsh"
 env ZSH=$ZSH sh $ZSH/tools/upgrade.sh
-
-echo "Updating VIM"
-cd ~/.vim_runtime
-git pull --rebase
-
-echo "Updating Tmux"
-cd ~/.tmux
-git pull --rebase
